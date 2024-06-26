d72793ff61f61b4d5e0f07023d2c9cdc9fb8244a
Luiz Augusto da S. Oliveira <loliveira@PSG.LOCAL>
2024-06-26 08:27:37 -0400

    Firsti Commit  Primeiro treinamento Completo

---
+++ api/src/main/java/med/voll/api/controller/PacienteController.java
@@ -0,0 +1,28 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.paciente.DadosCadastroPaciente;
+import med.voll.api.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
+
+public class PacienteController {
+   @Autowired
+   private PacienteRepository repository;
+
+    @PostMapping
+    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
+        // System.out.println("dados recebidos: "+dados);
+    }
+
+    @GetMapping
+    public Page<DadosListagemPaciente> listar(@PageableDefault(page = 0, size = 10, sort = {"nome"}) Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemPaciente::new);
+    }
+}
